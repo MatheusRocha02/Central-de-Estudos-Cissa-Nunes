6afff42361ca11d96488478d037189855ddf00fc
Matheus Rocha <matheusdcrocha15@gmail.com>
2024-07-08 08:55:36 -0300

    Segundo commit

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 
-//Mobile menu
+//*****************Mobile menu***************
 function showAndHideMobileMenu() {
     const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
     const mobileMenu = document.querySelector(".mobile-menu ul");
@@ -7,7 +7,7 @@ function showAndHideMobileMenu() {
     const line2 = document.querySelector(".line2");
     const line3 = document.querySelector(".line3");
 
-    mobileMenuIcon.addEventListener("click", function () {
+    mobileMenuIcon.addEventListener("click", function () { //Altera a classe das divs que formam o ícone para viraram o "X"
         mobileMenu.classList.toggle("show");
 
         line1.classList.toggle("line1-changed");
@@ -19,15 +19,7 @@ function showAndHideMobileMenu() {
 showAndHideMobileMenu();
 
 
-//Mobile Menu Icon
-function changeIcon() {
-    
-
-
-}
-
-
-//Envio do Formulário
+//****************Envio do Formulário**********
 const formulario = document.querySelector(".formulario-de-inscricao");
 
 formulario.addEventListener('submit', function (event) {
@@ -47,4 +39,44 @@ formulario.addEventListener('submit', function (event) {
 });
 
 
+//****** Slider "Aulas Oferecidas" *******/
+let slideIndex = 0;
 
+function changeSlide(n) {
+    slideIndex += n;
+    const cards = document.querySelectorAll(".card");
+    if (slideIndex >= cards.length) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = cards.length - 1;
+    }
+    showSlide(slideIndex);
+}
+
+function showSlide(n){
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach((card, index) => {
+        if (index === n){
+            card.style.display = 'flex';
+        } else{
+            card.style.display = 'none';
+        } 
+    });
+}
+
+//Mostra e tira seção "Ver todas"
+
+const verTodasBtn = document.querySelector(".ver-todas-btn");
+const SectionVerTodas = document.querySelector(".ver-todas");
+
+function changeVisibility() {
+    SectionVerTodas.classList.toggle("appear");
+    verTodasBtn.textContent = verTodasBtn.textContent === "Ver todas >" ? "< Fechar" : "Ver todas >";
+
+    if (SectionVerTodas.classList.contains("appear")) {
+        SectionVerTodas.scrollIntoView({ behavior: "smooth" });
+    }
+}
+
+verTodasBtn.addEventListener("click",changeVisibility);
